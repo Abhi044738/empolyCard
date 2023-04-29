e97078dffa11a8de12b88a9fd857b471e1bcf89b
Abhi044738 <110322117+Abhi044738@users.noreply.github.com>
2023-04-29 18:00:22 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import { EmpolyCard } from "./EmpolyCard";
+import "./styles.css";
+export default function App() {
+  return (
+    <div className="App">
+      <EmpolyCard />
+    </div>
+  );
+}
---
+++ src/EmpolyCard.js
@@ -0,0 +1,17 @@
+export function EmpolyCard() {
+  return (
+    <div>
+      <h1>Aditya</h1>
+      <div>
+        <p>
+          <span style={{ color: "red" }}>Designation:</span> Senior Software
+          Engineer
+        </p>
+        <p>
+          <span style={{ color: "blue" }}>Experience:</span>{" "}
+          <span style={{ font: "18px" }}> 10 years</span>
+        </p>
+      </div>
+    </div>
+  );
+}
